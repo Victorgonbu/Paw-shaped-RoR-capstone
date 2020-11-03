1a5c3587e52ee144d50f3adb3ff33f540baf6587
Shaqri <peter200970@hotmail.com>
2020-11-03 01:01:59 -0500

    Add css style for navbar

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,6 @@
  class CategoriesController < ApplicationController
   def index
-    @categories = Category.order(priority: :asc)
+    @categories = Category.ordered
     @most_voted = Vote.most_liked
     @most_voted = Post.find(@most_voted)
 
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,6 @@
 class Category < ApplicationRecord
   has_many :posts
   has_many :votes, through: :posts
+
+  scope :ordered, -> { order(priority: :asc) }
 end
