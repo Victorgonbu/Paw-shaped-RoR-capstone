e0bb394029acc9060bba36fd5730e8d5909f4361
Shaqri <peter200970@hotmail.com>
2020-11-03 17:23:07 -0500

    Add css style for root path

--- app/helpers/paws_helper.rb
+++ app/helpers/paws_helper.rb
@@ -1,5 +1,5 @@
 module PawsHelper
   def buy_paws(number)
-    link_to("Buy #{number} paws", user_paws_path(user_id: current_user.id, paws: number), method: :post)
+    link_to("Get #{number} paws x $#{number * 10} USD", user_paws_path(user_id: current_user.id, paws: number), method: :post)
   end
 end
