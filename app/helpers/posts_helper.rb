468532632c717530e2774e7889d3dadf4d7839ae
Shaqri <peter200970@hotmail.com>
2020-11-05 00:49:15 -0500

    Refactor posts views into helpers and partials

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,13 @@
 module PostsHelper
+  def paw_post_status(post)
+    if post.goal < 1 && post.paws.any?
+      render '/posts/goal_completed', post: post
+    else
+      render '/posts/goal_in_progress', post: post
+    end
+  end
+
+  def goal_status(post)
+    render '/posts/goal_remaining' if post.goal > 0
+  end
 end
