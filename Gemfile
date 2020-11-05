7a1432f5d1e0d94c46148088de773f91956402ce
Shaqri <peter200970@hotmail.com>
2020-11-05 17:51:20 -0500

    Add rspec user model tests

--- Gemfile
+++ Gemfile
@@ -36,6 +36,8 @@ gem 'rubocop'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: %i[mri mingw x64_mingw]
+  gem "database_cleaner"
+  gem "rspec-rails"
 end
 
 group :development do
@@ -46,6 +48,16 @@ group :development do
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
 end
+
+group :test do
+  # Adds support for Capybara system testing and selenium driver
+  gem 'capybara', '~> 3.33'
+  gem 'rspec', '~> 3.9'
+  gem 'selenium-webdriver', '~> 3.142', '>= 3.142.7'
+  # Easy installation and use of web drivers to run system tests with browsers
+  gem 'webdrivers'
+end
+
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: %i[mingw mswin x64_mingw jruby]
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,48 @@
+require 'rails_helper'
+
+RSpec.describe User do
+  let(:user) do
+    User.create(name: 'victor', email: 'victor@victor.com')
+  end
+
+  describe 'Associations' do
+    it 'has many paws' do
+      user = User.reflect_on_association(:paws)
+      expect(user.macro).to eq(:has_many)
+    end
+    it 'has many votes' do
+      user = User.reflect_on_association(:votes)
+      expect(user.macro).to eq(:has_many)
+    end
+    it 'has many posts' do
+      user = User.reflect_on_association(:posts)
+      expect(user.macro).to eq(:has_many)
+    end
+  end
+
+  describe 'Validations' do
+    describe 'name' do
+      it 'muts be unique' do
+        user
+        expect(User.create(name: 'victor', email: 'victor@victormanuel.com')).to_not be_valid
+      end
+    end
+
+    describe 'email' do
+      it 'must be valid' do
+        user.email = 'vic'
+        expect(user).to_not be_valid
+      end
+
+      it 'must be unique' do
+        user
+        expect(User.create(name: 'manuel', email: 'victor@victor.com')).to_not be_valid
+      end
+
+      it 'is valid' do
+        expect(user).to be_valid
+      end
+
+    end
+  end
+end
